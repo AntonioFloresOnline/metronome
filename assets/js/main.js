c5b8a9219fc31a6e8b11c8a4beb3153758b3787f
antoniofloresonline <wanllyf@gmail.com>
2020-08-29 22:54:42 -0600

    Calling assets from external files and implementing the metronome logic

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+// Variables to function
+const startButton = document.getElementById("start-button");
+const stopButton = document.getElementById("stop-button");
+const beepSound = new Audio("assets/sounds/beep.wav");
+const bpmInput = document.getElementById("bpm");
+const displayBpm = document.getElementById("display-bpm");
+const runningMessage = document.getElementById("running-message");
+
+// Metronome Object
+const metronome = {
+  bpm: 60,
+  compass: "4/4",
+  running: false,
+  interval: "?",
+  start() {
+    if (bpmInput.value != "") {
+      this.bpm = bpmInput.value;
+      this.interval = 60000 / this.bpm;
+      if (this.running == false) {
+        this.running = setInterval(() => {
+          beepSound.play();
+        }, this.interval);
+        console.log(`Metronome started at: ${this.bpm}`);
+        displayBpm.textContent = this.bpm;
+        runningMessage.textContent = `Metronome started at: ${this.bpm}`;
+      }
+    } else {
+      runningMessage.textContent = `Please type a value between 60 and 300 bpm`;
+    }
+  },
+  stop() {
+    clearInterval(this.running);
+    this.running = false;
+    console.log("Metronomo stopped");
+    runningMessage.textContent = `Metronome Stopped`;
+  },
+};
+
+// User Interaction
+function startMetronome() {
+  metronome.start();
+}
+
+function stopMetronome() {
+  metronome.stop();
+}
+
+startButton.addEventListener("click", startMetronome);
+stopButton.addEventListener("click", stopMetronome);
